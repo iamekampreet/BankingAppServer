2f5637e03431df6571c10007fc450cf8254da834
Santosh Dhakal <santosh.dhakal07@gmail.com>
2023-07-15 20:11:46 -0400

    Modified user schema and added transaction schema

---
+++ models/transaction-schema.js
@@ -0,0 +1,38 @@
+import mongoose, { SchemaTypes } from "mongoose";
+
+/*
+    My Assumptions;
+    - I am assuming checking account and debit card are same.
+    - So, a debit card number will be same as checking account number
+    - Any transaction performed using debit card will be an checking account transaction
+    - Money must be moved from saving to checking before 
+        it can go to other user's account
+
+    - Transactions types;
+        - saving to checking(same account)
+        - checking to saving(same account)
+        - debit/checking to another user's checking account
+        - credit to another user(business or regular) checking account
+*/
+
+/*
+    - This collection will be huge, so we can create an index on accountNumber 
+*/
+
+const TransactionSchema = new mongoose.Schema({
+    // origin from user can be checking, saving/debit, credit
+    accountNumber: { type: BigInt, required: true },
+
+    // debit(incoming) or credit(outgoing) (so, no need for different incoming and outgoing transactin types)
+    transactionType: { type: String, required: true },
+    // if debit(incoming) will represent from which account
+    // if credit(outgoing) will represent to which account
+    // *the name can be improved
+    transactionToFrom: { type: BigInt, required: true},
+    // i don't think rounding money is good idea :)
+    amount: { type: SchemaTypes.Decimal128, required: true },
+    currency: { type: String, required: true },
+    transactionTime: { type: Date, required: true },
+    // pending/complete: will be used in split bill and request payment features
+    status: { type: String, required: true}
+})
---
+++ models/user-schema.js
@@ -0,0 +1,59 @@
+import { SchemaTypes } from 'mongoose'
+import mongoose from '../utils/database.js'
+
+// Moved transaction to a different collection
+// Mongodb has max document size limit of around 16mb.
+// Transaction is an unbounded field, so it will increase 
+// and the document size will increse with each transaction
+// Also, we don't want all the transaction history to be returned when
+// we are just querying for the User info. (I'm assuming everything will be returned.)
+
+const UserSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    // unique is used to make index for faster querying, not validation
+    email: { type: String, required: true, unique: true, match: /.+\@.+\..+/ },
+    password: { type: String, required: true, min: 8 },
+    address: { type: String, required: true },
+    phone: { type: String, required: true },
+    sin_number: { type: String, required: true },
+    //can be personal, student, business
+    accountType: { type: String, required: true },
+    //for business users
+    displayName: { type: String, required: true},
+    // Using only checking and saving will make us limited to these accounts only
+    // If we use list we can add other account types like joint checking account 
+    accounts: [
+        {
+            accountNumber: { type: BigInt, required: true },
+            // 1 - Saving, 2 - checking, 3- Joint, 4 - some other future types
+            //Int32 (efficient for indexing)
+            accountType: { type: Number, required: true },
+            enabled: { type: Boolean, required: true },
+            accountBalance: {
+                type: SchemaTypes.Decimal128, required: true
+            },
+            //for joint-checking like accounts
+            owners: { type: String, required: false }
+        }
+    ],
+    //debit card, credit card
+    cards: [
+        {
+            cardType: { type: String, required: true },
+            cardNumber: { type: BigInt, required: true },
+            expiryDate:  { type: Date, required: true },
+            securityCode: { type: String, required: true },
+            enabled: { type: Boolean, required: true },
+            maxLimit: { type: SchemaTypes.Decimal128, required: true },
+            accountBalance: {
+                type: SchemaTypes.Decimal128, required: true
+            },
+            //can add sub types of credit cards like student card, personal, business
+        }
+    ]
+});
+
+const User = mongoose.model('User', UserSchema)
+
+export default User
