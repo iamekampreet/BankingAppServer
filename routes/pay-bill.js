6e65fdbce5b44e0143e5f8a3cb7826c7b4fd4463
Santosh Dhakal <santosh.dhakal07@gmail.com>
2023-08-16 14:08:56 -0400

    feat: add stop payment

--- routes/pay-bill.js
+++ routes/pay-bill.js
@@ -11,4 +11,6 @@ router.route("/updateUserPayee").post(payBillController.updateUserPayee);
 
 router.route("/upcoming-payments").get(payBillController.upcomingPayments);
 
+router.route("/stop-payments").post(payBillController.stopPayment);
+
 module.exports = router;
